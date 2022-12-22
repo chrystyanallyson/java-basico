2277629953530e15590066f8c9e22e5a4995f242
Chrystyan allyson <chrystyan.allyson@gmail.com>
2022-12-22 10:06:44 -0300

    metodos

---
+++ metodos/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV LIGADA: " + smartTv.ligada);
+        System.out.println("CANAL: " + smartTv.canal);
+        System.out.println("VOLUME: " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("TV LIGADA: " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        
+        System.out.println("VOLUME: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("CANAL: " + smartTv.canal);
+    }
+}
