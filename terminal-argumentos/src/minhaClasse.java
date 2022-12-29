5b3d22055c91cbb40c6ca5a93e2459d2edad29c5
Chrystyan allyson <chrystyan.allyson@gmail.com>
2022-12-29 11:46:40 -0300

    .

--- terminal-argumentos/src/minhaClasse.java
+++ terminal-argumentos/src/minhaClasse.java
@@ -1,5 +1,5 @@
 public class  minhaClasse {
     public static void main(String[] args) {
-        System.out.println("Oi, fui executado pelo terminal");              
+        System.out.println("Oi, fui executado pelo terminal ");              
     }
 }
